d32e91d153fb099f8782ab08a8b896c529e220a1
Luke Thomas <lukethomasyig@gmail.com>
2024-05-02 03:53:55 +0000

    Version 0.4

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class CarFinder:
                 return [line.strip() for line in file.readlines()]
         except FileNotFoundError:
             print(f"No file found at {self.file_path}. Creating a new one with initial vehicles.")
-            initial_vehicles = ['Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan']
+            initial_vehicles = ['Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan', 'Rivian R1T', 'Ram 1500']
             with open(self.file_path, 'w') as file:
                 for vehicle in initial_vehicles:
                     file.write(vehicle + '\n')
@@ -34,7 +34,7 @@ class CarFinder:
             print(f"{vehicle_name.capitalize()} is an authorized vehicle")
         else:
             print(f"{vehicle_name.capitalize()} is not an authorized vehicle. If you received this in error, please check the spelling and try again.")
-
+# fileEdit Add Vehicle
     def add_vehicle(self, vehicle_name):
         vehicle_name = vehicle_name.strip()
         if vehicle_name not in self.AllowedVehiclesList:
@@ -43,17 +43,29 @@ class CarFinder:
             self.save_AllowedVehiclesList()
         else:
             print(f'"{vehicle_name}" is already an authorized vehicle')
+#fileEdit Delete Vehicle
+    def delete_vehicle(self, vehicle_name):
+        vehicle_name = vehicle_name.strip()
+        if vehicle_name in self.AllowedVehiclesList:
+            confirm = input(f'Are you sure you want to remove "{vehicle_name}" from the Authorized Vehicles List? (yes/no): ')
+            if confirm.lower() == 'yes':
+                self.AllowedVehiclesList.remove(vehicle_name)
+                print(f'You have REMOVED "{vehicle_name}" as an authorized vehicle')
+                self.save_AllowedVehiclesList()
+        else:
+            print(f'"{vehicle_name}" is not found in the Authorized Vehicles List')
   # displayText Choice Menu
     def display_menu(self):
         print("********************************")
-        print("AutoCountry Vehicle Finder v0.3")
+        print("AutoCountry Vehicle Finder v0.4")
         print("********************************")
         print("Please enter the following number below from the following menu:\n")
         print("1. PRINT all Authorized Vehicles")
         print("2. SEARCH for Authorized Vehicle")
         print("3. ADD Authorized Vehicle")
-        print("4. Exit")
-  # computeDecision 1 OR 2 OR 3 OR 4
+        print("4. DELETE Authorized Vehicle")
+        print("5. Exit")
+
     def run(self):
         while True:
             self.display_menu()
@@ -68,6 +80,9 @@ class CarFinder:
                 vehicle_name = input("Please enter the full Vehicle name you would like to add: ")
                 self.add_vehicle(vehicle_name)
             elif choice == '4':
+                vehicle_name = input("Please enter the full Vehicle name you would like to remove: ")
+                self.delete_vehicle(vehicle_name)
+            elif choice == '5':
                 print("Thank you for using the AutoCountry Vehicle Finder, goodbye!")
                 self.save_AllowedVehiclesList()  # Save changes before exiting
                 sys.exit()
