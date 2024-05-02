5f470af289f6e3cae76e8c27f687a9b5211bb161
Luke Thomas <lukethomasyig@gmail.com>
2024-05-02 03:08:41 +0000

    Version 0.3

--- main.py
+++ main.py
@@ -10,21 +10,27 @@ class CarFinder:
             print(vehicle)
 
     def search_vehicle(self, vehicle_name):
-        vehicle_name = vehicle_name.lower()  # convertInput type lowercase
+      # convertInput type lowercase
+        vehicle_name = vehicle_name.lower()
         if vehicle_name in [v.lower() for v in self.AllowedVehiclesList]:
             print(f"{vehicle_name.capitalize()} is an authorized vehicle")
         else:
             print(f"{vehicle_name.capitalize()} is not an authorized vehicle. If you received this in error, please check the spelling and try again.")
-# displayText Choice Menu
+
+    def add_vehicle(self, vehicle_name):
+        self.AllowedVehiclesList.append(vehicle_name)
+        print(f'You have added "{vehicle_name}" as an authorized vehicle')
+  # displayText Choice Menu
     def display_menu(self):
         print("********************************")
-        print("AutoCountry Vehicle Finder v0.2")
+        print("AutoCountry Vehicle Finder v0.3")
         print("********************************")
         print("Please enter the following number below from the following menu:\n")
         print("1. PRINT all Authorized Vehicles")
         print("2. SEARCH for Authorized Vehicle")
-        print("3. Exit")
-# computeDecision 1 OR 2 OR 3
+        print("3. ADD Authorized Vehicle")
+        print("4. Exit")
+  # computeDecision 1 OR 2 OR 3 OR 4
     def run(self):
         while True:
             self.display_menu()
@@ -36,6 +42,9 @@ class CarFinder:
                 vehicle_name = input("Please enter the full Vehicle name: ")
                 self.search_vehicle(vehicle_name)
             elif choice == '3':
+                vehicle_name = input("Please enter the full Vehicle name you would like to add: ")
+                self.add_vehicle(vehicle_name)
+            elif choice == '4':
                 print("Thank you for using the AutoCountry Vehicle Finder, goodbye!")
                 sys.exit()
             else:
